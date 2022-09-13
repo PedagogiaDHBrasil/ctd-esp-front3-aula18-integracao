a9c5220e17a7f86f2f1f9a25740b718e09c546ab
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-09-13 02:33:14 -0300

    Add files via upload

---
+++ pages/api/db.ts
@@ -0,0 +1,146 @@
+export const students = [
+  {
+    id: "1",
+    nome: "Steve",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "6",
+      segundo: "10",
+    },
+  },
+  {
+    id: "2",
+    nome: "Kolos",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "10",
+      segundo: "9",
+    },
+  },
+  {
+    id: "3",
+    nome: "Tomy",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "10",
+      segundo: "10",
+    },
+  },
+  {
+    id: "4",
+    nome: "Ani",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "6",
+      segundo: "5",
+    },
+  },
+  {
+    id: "5",
+    nome: "Sergio",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "7",
+      segundo: "6",
+    },
+  },
+  {
+    id: "6",
+    nome: "Uri",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "10",
+      segundo: "8",
+    },
+  },
+  {
+    id: "7",
+    nome: "Felipe",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "8",
+      segundo: "3",
+    },
+  },
+  {
+    id: "8",
+    nome: "Jorge",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "8",
+      segundo: "4",
+    },
+  },
+  {
+    id: "9",
+    nome: "Javi",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "4",
+      segundo: "10",
+    },
+  },
+  {
+    id: "10",
+    nome: "Joan",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "9",
+      segundo: "7",
+    },
+  },
+  {
+    id: "11",
+    nome: "Mati",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "8",
+      segundo: "8",
+    },
+  },
+  {
+    id: "12",
+    nome: "Fran",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "9",
+      segundo: "2",
+    },
+  },
+  {
+    id: "13",
+    nome: "Aiti",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "4",
+      segundo: "8",
+    },
+  },
+  {
+    id: "13",
+    nome: "Aldana",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "6",
+      segundo: "7",
+    },
+  },
+  {
+    id: "14",
+    nome: "Caro",
+    curso: "CTD",
+    avaliacoes: {
+      primeiro: "9",
+      segundo: "9",
+    },
+  },
+  {
+    id: "15",
+    nome: "Clara",
+    curso: "FullStack",
+    avaliacoes: {
+      primeiro: "3",
+      segundo: "7",
+    },
+  },
+];
